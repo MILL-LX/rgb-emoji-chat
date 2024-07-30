8110b2614505495f9ecbdc39d6cee25067e034bc
Mo Mobarak <mo@mobarak.com>
2024-07-30 18:17:09 +0100

    a super basic chat app

---
+++ app/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, join_room, leave_room, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    send(msg, broadcast=True)
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(f'{username} has entered the room.', to=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(f'{username} has left the room.', to=room)
+
+if __name__ == '__main__':
+    # Specify the IP address and port
+    socketio.run(app, host='10.10.10.52', port=3000)

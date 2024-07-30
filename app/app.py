774957c85c9c7dd869f298c3e052dba7da19417b
Mo Mobarak <mo@mobarak.com>
2024-07-30 15:55:03 +0100

    some more experimenting with the python FT interface

--- app/app.py
+++ app/app.py
@@ -1,24 +1,39 @@
-import flaschen
+import math
+import random
 import time
+
+import lib.flaschen as flaschen
 
 UDP_IP = 'localhost'
 UDP_PORT = 1337
-rows = 64
-cols = 64
+height = 64
+width = 64
 
-ft = flaschen.Flaschen(UDP_IP, UDP_PORT, cols, rows)
+ft = flaschen.Flaschen(UDP_IP, UDP_PORT, width, height)
 
-r = 0
-g = 0
-b = 0
+def distance(x, y, width, height):
+    return math.sqrt((width - x)**2 + (height - y)**2)
+
+far_corner_distance = distance(0,0,width,height)
 while True:
     for y in range(0, ft.height):
         for x in range(0, ft.width):
+            dist_color = int(distance(x, y, width, height) / far_corner_distance * 255)
+            r = dist_color
+            g = dist_color
+            b = dist_color
+            color = (r,g,b)
             ft.set(x, y, (r,g,b))
     ft.send()
-
     time.sleep(3)
 
-    r = (r + 10) % 256
-    g = (r + 20) % 256
-    b = (r + 14) % 256
+    for y in range(0, ft.height):
+        for x in range(0, ft.width):
+            dist_color = 255 - int(distance(x, y, width, height) / far_corner_distance * 255)
+            r = dist_color
+            g = dist_color
+            b = dist_color
+            color = (r,g,b)
+            ft.set(x, y, (r,g,b))
+    ft.send()
+    time.sleep(3)

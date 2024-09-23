44b979a5c88baafc7f62643958445e5e685e028a
Mo Mobarak <mo@mobarak.com>
2024-09-23 15:15:11 +0100

    not using logos before showing image, sending event on queue update

--- app/app_modules/image_queue_manager.py
+++ app/app_modules/image_queue_manager.py
@@ -11,9 +11,6 @@ from app_modules.image_util import image_for_code
 from app_modules.display import Display
 from app_modules.pi_util import is_raspberry_pi
 
-LOCAL_DEVELOPMENT_LOGO_IMAGE_CODES = ['1f1e6-1f1eb', '1f3c2-1f3fb']
-LOGO_IMAGE_CODES = ['MH G Collection PART/8', 'MH G Collection PART/9']  if is_raspberry_pi() else LOCAL_DEVELOPMENT_LOGO_IMAGE_CODES #TODO: substitute with actual logo image codes
-LOGO_IMAGE_DURATION = 2
 IMAGE_DURATION = 10 #TODO: determine actual image duration
 
 class ImageQueueManager:
@@ -21,8 +18,6 @@ class ImageQueueManager:
         self.image_queue = Queue()
         self.display = display
         self.socketio = socketio
-        logo_image_codes = LOGO_IMAGE_CODES
-        self.logo_images = [image_for_code(code, display.size()) for code in logo_image_codes]
 
         self.current_image_code = None
 
@@ -33,6 +28,11 @@ class ImageQueueManager:
     def add_image(self, image_code):
         self.image_queue.put(image_code)
 
+        updated_at = time.time()
+        updated_at_str = datetime.datetime.fromtimestamp(updated_at, datetime.timezone.utc).isoformat() + 'Z'  # Use UTC time
+        event_data_json = json.dumps({'event': 'image_queue_updated', 'event_time': updated_at_str, 'image_code': image_code})
+        self.socketio.emit('image_updates', event_data_json)
+
     def display_images(self):
         while self.running:
             if not self.image_queue.empty():
@@ -40,18 +40,12 @@ class ImageQueueManager:
                 image = image_for_code(image_code, self.display.size())
                 if image:
                     self.current_image_code = image_code
-
-                    self.display.send_image(random.choice(self.logo_images)) # randomly select one of the logo images   
-                    time.sleep(LOGO_IMAGE_DURATION)
-
                     self.display.send_image(image)
 
                     updated_at = time.time()
                     updated_at_str = datetime.datetime.fromtimestamp(updated_at, datetime.timezone.utc).isoformat() + 'Z'  # Use UTC time
                     event_data_json = json.dumps({'event': 'image_updated', 'event_time': updated_at_str, 'image_code': image_code})
-
                     self.socketio.emit('image_updates', event_data_json)
-                    self.socketio.emit('message', event_data_json) #DEBUG: we debug by publishing to the chat room, remove this   
                     
                     time.sleep(IMAGE_DURATION)
 
